c8e28a8e8c7bc51b5f94c8f6b76e0867e6528de6
cwher <cwher@outlook.com>
2022-05-01 12:57:19 +0800

    to: add memtable

---
+++ src/memtable.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "skiplist.hpp"
+
+class MemTable
+{
+    friend class SSTable;
+    // 2 MB
+    static const u32 MAX_SIZE = 1 << 21;
+
+private:
+    SkipList<u64, string> skip_list;
+    u32 current_size = 0;
+
+public:
+    void clear()
+    {
+        current_size = 0;
+        skip_list.clear();
+    }
+
+    u32 size() { return skip_list.size(); }
+    bool empty() { return size() == 0; }
+
+    bool insert(const u64 &key, const string &value)
+    {
+        auto result = at(key);
+        // (key: u64) + (offset: u32) + (value: string)
+        u32 entry_size = !result.first
+                             ? 12 + value.length()
+                             : value.length() - result.second.length();
+        if (current_size + entry_size > MAX_SIZE)
+            return false; // overflow
+
+        current_size += entry_size;
+        skip_list.insert(key, value);
+    }
+
+    void remove(const u64 &key)
+    {
+        skip_list.remove(key);
+    }
+
+    pair<bool, string> at(const u64 &key)
+    {
+        return skip_list.at(key);
+    }
+};

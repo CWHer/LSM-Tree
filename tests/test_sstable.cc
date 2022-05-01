a462caf80224d68640e88998058969e472aa3054
cwher <cwher@outlook.com>
2022-05-01 19:02:43 +0800

    test: add sstable test

---
+++ tests/test_sstable.cc
@@ -0,0 +1,26 @@
+#include "memtable.hpp"
+#include "sstable.hpp"
+
+MemTable mem_table;
+
+int main()
+{
+    printInfo("Test1");
+    for (i32 i = 0; i < 20; ++i)
+    {
+        mem_table.insert(i, std::to_string(i));
+        std::cout << mem_table.at(i).second << '\n';
+    }
+    std::cout << mem_table.size() << ' '
+              << mem_table.byteSize() << '\n';
+    SSTable sstable(mem_table, 0);
+    mem_table.clear();
+    std::cout << mem_table.size() << ' '
+              << mem_table.byteSize() << '\n';
+    for (i32 i = 0; i < 25; ++i)
+        std::cout << i << ':'
+                  << sstable.find(i).second << '\n';
+
+    std::cout << std::endl;
+    return 0;
+}
